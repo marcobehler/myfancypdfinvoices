26d21b64877a1300a9eea9c5fd1e253d4cfaa88f
Marco Behler <marco@marcobehler.com>
2020-06-19 13:33:20 +0100

    Rendering HTML pages

--- src/main/java/com/marcobehler/MyFirstServlet.java
+++ src/main/java/com/marcobehler/MyFirstServlet.java
@@ -10,11 +10,12 @@ public class MyFirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html; charset=UTF-8");
-        resp.getWriter().print("<html>\n" +
-                        "<body>\n" +
-                        "<h1>Hello World</h1>\n" +
-                        "<p>This is my very first, embedded Tomcat, HTML Page!</p>\n" +
+        resp.getWriter().print(
+                "<html>\n" +
+                "<body>\n" +
+                "<h1>Hello World</h1>\n" +
+                "<p>This is my very first, embedded Tomcat, HTML Page!</p>\n" +
                 "</body>\n" +
-                        "</html>");
+                "</html>");
     }
 }
