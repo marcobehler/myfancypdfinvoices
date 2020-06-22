b01261da15e9caa583c2a77636028fa363919b3b
Marco Behler <marco@marcobehler.com>
2020-06-22 15:22:36 +0100

    Dependency Injection

---
+++ src/main/java/com/marcobehler/myfancypdfinvoices/services/UserService.java
@@ -0,0 +1,14 @@
+package com.marcobehler.myfancypdfinvoices.services;
+
+import com.marcobehler.myfancypdfinvoices.model.User;
+
+import java.util.UUID;
+
+public class UserService {
+
+    public User findById(String id) {
+        String randomName = UUID.randomUUID().toString();
+        // always "finds" the user, every user has a random name
+        return new User(id, randomName);
+    }
+}
