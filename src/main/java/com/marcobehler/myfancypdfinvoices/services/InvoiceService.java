95d5bce91b334f9cd561edf818dd927f0471149e
Marco Behler <marco@marcobehler.com>
2020-06-22 15:24:30 +0100

    Dependency Injection

--- src/main/java/com/marcobehler/myfancypdfinvoices/services/InvoiceService.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/services/InvoiceService.java
@@ -1,19 +1,26 @@
 package com.marcobehler.myfancypdfinvoices.services;
 
 import com.marcobehler.myfancypdfinvoices.model.Invoice;
+import com.marcobehler.myfancypdfinvoices.model.User;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class InvoiceService {
 
-    List<Invoice> invoices = new CopyOnWriteArrayList<>(); // <1>
+    List<Invoice> invoices = new CopyOnWriteArrayList<>();
 
     public List<Invoice> findAll() {
         return invoices;
     }
 
     public Invoice create(String userId, Integer amount) {
+        User user = new UserService().findById(userId);
+        if (user == null) {
+            throw new IllegalStateException();
+        }
+
+
         // TODO real pdf creation and storing it on network server
         Invoice invoice = new Invoice(userId, amount, "http://www.africau.edu/images/default/sample.pdf");
         invoices.add(invoice);
