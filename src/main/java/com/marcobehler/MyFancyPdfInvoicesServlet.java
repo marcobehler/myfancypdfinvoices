b0ad41808784f54c2f4633532e41a3b2b4ff92ed
Marco Behler <marco@marcobehler.com>
2020-06-22 09:33:33 +0100

    JSON Endpoint REST Services

--- src/main/java/com/marcobehler/MyFancyPdfInvoicesServlet.java
+++ src/main/java/com/marcobehler/MyFancyPdfInvoicesServlet.java
@@ -1,11 +1,30 @@
 package com.marcobehler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class MyFancyPdfInvoicesServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (request.getRequestURI().equalsIgnoreCase("/invoices")) {
+
+            String userId = request.getParameter("user_id");
+            Integer amount = Integer.valueOf(request.getParameter("amount"));
+
+            Invoice invoice = new InvoiceService().create(userId, amount);
+
+            response.setContentType("application/json; charset=UTF-8");
+            String json = new ObjectMapper().writeValueAsString(invoice);
+            response.getWriter().print(json);
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -18,8 +37,7 @@ public class MyFancyPdfInvoicesServlet extends HttpServlet {
                             "<p>This is my very first, embedded Tomcat, HTML Page!</p>\n" +
                             "</body>\n" +
                             "</html>");
-        }
-        else if (request.getRequestURI().equalsIgnoreCase("/invoices")) {
+        } else if (request.getRequestURI().equalsIgnoreCase("/invoices")) {
             response.setContentType("application/json; charset=UTF-8");
             response.getWriter().print("[]");
         }
