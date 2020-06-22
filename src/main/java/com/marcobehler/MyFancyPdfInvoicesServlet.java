4c6a8fdfedfb1974d0f1135c5127fa5d01104375
Marco Behler <marco@marcobehler.com>
2020-06-22 08:31:59 +0100

    JSON Endpoint REST Services

---
+++ src/main/java/com/marcobehler/MyFancyPdfInvoicesServlet.java
@@ -0,0 +1,27 @@
+package com.marcobehler;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyFancyPdfInvoicesServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (request.getRequestURI().equalsIgnoreCase("/")) {
+            response.setContentType("text/html; charset=UTF-8");
+            response.getWriter().print(
+                    "<html>\n" +
+                            "<body>\n" +
+                            "<h1>Hello World</h1>\n" +
+                            "<p>This is my very first, embedded Tomcat, HTML Page!</p>\n" +
+                            "</body>\n" +
+                            "</html>");
+        }
+        else if (request.getRequestURI().equalsIgnoreCase("/invoices")) {
+            response.setContentType("application/json; charset=UTF-8");
+            response.getWriter().print("[]");
+        }
+    }
+}
