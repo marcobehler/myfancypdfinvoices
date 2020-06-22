64a11a9a5ea9a3351bc4dcf1048f34bafc342825
Marco Behler <marco@marcobehler.com>
2020-06-22 15:27:01 +0100

    Dependency Injection

--- src/main/java/com/marcobehler/myfancypdfinvoices/context/Application.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/context/Application.java
@@ -2,10 +2,11 @@ package com.marcobehler.myfancypdfinvoices.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.marcobehler.myfancypdfinvoices.services.InvoiceService;
+import com.marcobehler.myfancypdfinvoices.services.UserService;
 
 public class Application {
 
     public static final InvoiceService invoiceService = new InvoiceService();
     public static final ObjectMapper objectMapper = new ObjectMapper();
-
+    public static final UserService userService = new UserService();
 }
