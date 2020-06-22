d8089c6579f21c1278b08bc7ff5d4ac1383b871e
Marco Behler <marco@marcobehler.com>
2020-06-22 09:59:27 +0100

    JSON Endpoint REST Services

--- src/main/java/com/marcobehler/InvoiceService.java
+++ src/main/java/com/marcobehler/InvoiceService.java
@@ -1,10 +1,20 @@
 package com.marcobehler;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class InvoiceService {
+
+    List<Invoice> invoices = new CopyOnWriteArrayList<>(); // <1>
+
+    public List<Invoice> findAll() {
+        return invoices;
+    }
 
     public Invoice create(String userId, Integer amount) {
         // TODO real pdf creation and storing it on network server
-        return new Invoice(userId, amount, "http://www.africau.edu/images/default/sample.pdf");
+        Invoice invoice = new Invoice(userId, amount, "http://www.africau.edu/images/default/sample.pdf");
+        invoices.add(invoice);
+        return invoice;
     }
-
 }
