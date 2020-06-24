a2f93dbe69a39e1db97208c682687c61b811211a
Marco Behler <marco@marcobehler.com>
2020-06-24 13:51:38 +0100

    Spring DI

---
+++ src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -0,0 +1,7 @@
+package com.marcobehler.myfancypdfinvoices.context;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyFancyPdfInvoicesApplicationConfiguration {
+}
