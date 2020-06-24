152c49d6d6890f89e2bde4dcc6efa4c31d4eeeb4
Marco Behler <marco@marcobehler.com>
2020-06-24 13:57:48 +0100

    Spring DI

--- src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -1,7 +1,32 @@
 package com.marcobehler.myfancypdfinvoices.context;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.marcobehler.myfancypdfinvoices.services.InvoiceService;
+import com.marcobehler.myfancypdfinvoices.services.UserService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyFancyPdfInvoicesApplicationConfiguration {
+
+    // tag::userServiceMethod[]
+    @Bean
+    public UserService userService() {
+        return new UserService();
+    }
+    // end::userServiceMethod[]
+
+    // tag::invoiceServiceMethod[]
+    @Bean
+    public InvoiceService invoiceService(UserService userService) {
+        return new InvoiceService(userService);
+    }
+    // end::invoiceServiceMethod[]
+
+    // tag::objectMapperMethod[]
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+    // end::objectMapperMethod[]
 }
