298c41019b252d7c6d54abfb36c9a0bc4dfc9367
Marco Behler <marco@marcobehler.com>
2020-06-26 08:57:30 +0100

    Spring DI

--- src/main/java/com/marcobehler/myfancypdfinvoices/service/InvoiceService.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/service/InvoiceService.java
@@ -7,9 +7,10 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+// tag:atComponent[]
 @Component
 public class InvoiceService {
-
+// end:atComponent[]
     private final UserService userService;
 
     private List<Invoice> invoices = new CopyOnWriteArrayList<>();
