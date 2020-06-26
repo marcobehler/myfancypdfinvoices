33d9fb71156e84c5b1b205407a943fc6eb90b371
Marco Behler <marco@marcobehler.com>
2020-06-26 08:57:30 +0100

    Spring DI

--- src/main/java/com/marcobehler/myfancypdfinvoices/service/UserService.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/service/UserService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+// tag:atComponent[]
 @Component
+// end:atComponent[]
 public class UserService {
 
     public User findById(String id) {
