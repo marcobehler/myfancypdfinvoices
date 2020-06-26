916925372c93c3f346bc8742f8a2599cf0763c5c
Marco Behler <marco@marcobehler.com>
2020-06-26 08:39:42 +0100

    Spring DI

--- src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -1,30 +1,15 @@
 package com.marcobehler.myfancypdfinvoices.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.marcobehler.myfancypdfinvoices.service.InvoiceService;
-import com.marcobehler.myfancypdfinvoices.service.UserService;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
+// tag::componentScanAnnotation[]
+@ComponentScan
 public class MyFancyPdfInvoicesApplicationConfiguration {
-
-    // tag::userServiceMethod[]
-    @Bean
-    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public UserService userService() {
-        return new UserService();
-    }
-    // end::userServiceMethod[]
-
-    // tag::invoiceServiceMethod[]
-    @Bean
-    public InvoiceService invoiceService(UserService userService) {
-        return new InvoiceService(userService);
-    }
-    // end::invoiceServiceMethod[]
+// end::componentScanAnnotation[]
 
     // tag::objectMapperMethod[]
     @Bean
