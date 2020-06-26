94fcebd10a09820dfa8e7ca229c7a4c65f805d34
Marco Behler <marco@marcobehler.com>
2020-06-26 09:56:25 +0100

    Spring DI

--- src/main/java/com/marcobehler/myfancypdfinvoices/service/InvoiceService.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/service/InvoiceService.java
@@ -4,6 +4,7 @@ package com.marcobehler.myfancypdfinvoices.service;
 
 import com.marcobehler.myfancypdfinvoices.model.Invoice;
 import com.marcobehler.myfancypdfinvoices.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -16,6 +17,7 @@ public class InvoiceService {
 
     private final UserService userService;
 
+    @Autowired
     public InvoiceService(UserService userService) {
         this.userService = userService;
     }
