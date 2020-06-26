1ac371c3a3b49097652be2178e7a8e7d908eb64c
Marco Behler <marco@marcobehler.com>
2020-06-26 08:36:58 +0100

    Spring DI

--- src/main/java/com/marcobehler/myfancypdfinvoices/service/InvoiceService.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/service/InvoiceService.java
@@ -2,10 +2,12 @@ package com.marcobehler.myfancypdfinvoices.service;
 
 import com.marcobehler.myfancypdfinvoices.model.Invoice;
 import com.marcobehler.myfancypdfinvoices.model.User;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+@Component
 public class InvoiceService {
 
     private final UserService userService;
--- src/main/java/com/marcobehler/myfancypdfinvoices/service/UserService.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/service/UserService.java
@@ -1,9 +1,11 @@
 package com.marcobehler.myfancypdfinvoices.service;
 
 import com.marcobehler.myfancypdfinvoices.model.User;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+@Component
 public class UserService {
 
     public User findById(String id) {
