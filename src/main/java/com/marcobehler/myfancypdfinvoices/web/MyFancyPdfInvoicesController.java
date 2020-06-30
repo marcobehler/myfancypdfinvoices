a4060c0ef7189628cb1effc01276afe3e28d1bb8
Marco Behler <marco@marcobehler.com>
2020-06-30 12:35:53 +0100

    Spring Web MVC

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFancyPdfInvoicesController.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFancyPdfInvoicesController.java
@@ -3,13 +3,16 @@ package com.marcobehler.myfancypdfinvoices.web;
 import com.marcobehler.myfancypdfinvoices.model.Invoice;
 import com.marcobehler.myfancypdfinvoices.service.InvoiceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 // tag::restControllerAnnotation[]
-@RestController
+@Controller
+@ResponseBody
 public class MyFancyPdfInvoicesController {
 // end::restControllerAnnotation[]
 
