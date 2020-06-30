fd953cef534e9b392cca5f55eb954fc867062f2b
Marco Behler <marco@marcobehler.com>
2020-06-30 07:49:33 +0100

    Spring Web MVC

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFirstSpringController.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFirstSpringController.java
@@ -4,12 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+// tag::classAnnotations[]
 @Controller
 @ResponseBody
+// end:classAnnotations[]
 public class MyFirstSpringController {
 
+    // tag::getMapping[]
     @GetMapping("/")
+    // end::getMapping[]
+    // tag::indexMethod[]
     public String index() {
         return "Hello World";
     }
+    // end::indexMethod[]
 }
