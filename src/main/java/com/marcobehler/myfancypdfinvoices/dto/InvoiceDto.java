8fcb4291c19721ca5970dedfd269c3efad107e0e
Marco Behler <marco@marcobehler.com>
2020-07-01 09:52:23 +0100

    Spring Web MVC

--- src/main/java/com/marcobehler/myfancypdfinvoices/dto/InvoiceDto.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/dto/InvoiceDto.java
@@ -2,9 +2,12 @@ package com.marcobehler.myfancypdfinvoices.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class InvoiceDto {
 
     @JsonProperty("user_id")
+    @NotBlank
     private String userId;
 
     private Integer amount;
