51a4b429c752cca79359b07f1ee82f8814c3d76a
Marco Behler <marco@marcobehler.com>
2020-07-01 09:54:55 +0100

    Spring Web MVC

--- src/main/java/com/marcobehler/myfancypdfinvoices/dto/InvoiceDto.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/dto/InvoiceDto.java
@@ -8,13 +8,17 @@ import jakarta.validation.constraints.NotBlank;
 
 public class InvoiceDto {
 
+    // tag::notBlankValidation[]
     @JsonProperty("user_id")
     @NotBlank
     private String userId;
+    // end::notBlankValidation[]
 
+    // tag::minMaxValidation[]
     @Min(10)
     @Max(50)
     private Integer amount;
+    // end::minMaxValidation[]
 
     public String getUserId() {
         return userId;
