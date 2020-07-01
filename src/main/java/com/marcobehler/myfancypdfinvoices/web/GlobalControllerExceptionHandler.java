8fa9837f7579d80c4e34b8aa5f856c6993a7a6d9
Marco Behler <marco@marcobehler.com>
2020-07-01 10:56:29 +0100

    Spring Web MVC

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/GlobalControllerExceptionHandler.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/GlobalControllerExceptionHandler.java
@@ -9,20 +9,26 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import javax.validation.ConstraintViolationException;
 
 
+// tag::adviceAnnotation[]
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
+// end::adviceAnnotation[]
 
+    // tag::handlemethodArgumentNotValid[]
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public String handlemethodArgumentNotValid(MethodArgumentNotValidException exception) { // <4>
         // TODO you can choose to return your custom object here, which will then get transformed to json/xml etc.
         return "Sorry, that was not quite right: " + exception.getMessage();
     }
+    // end::handlemethodArgumentNotValid[]
 
+    // tag::handleConstraintViolation[]
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ConstraintViolationException.class)
     public String handleConstraintViolation(ConstraintViolationException exception) { // <4>
         // TODO you can choose to return your custom object here, which will then get transformed to json/xml etc.
         return "Sorry, that was not quite right: " + exception.getMessage();
     }
+    // end::handleConstraintViolation[]
 }
