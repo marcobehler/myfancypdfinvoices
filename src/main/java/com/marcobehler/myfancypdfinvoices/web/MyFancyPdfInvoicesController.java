99d55b9a937206386a5d86e765a35ab105b70bf5
Marco Behler <marco@marcobehler.com>
2020-07-01 10:16:45 +0100

    Spring Web MVC

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFancyPdfInvoicesController.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFancyPdfInvoicesController.java
@@ -3,12 +3,12 @@ package com.marcobehler.myfancypdfinvoices.web;
 import com.marcobehler.myfancypdfinvoices.model.Invoice;
 import com.marcobehler.myfancypdfinvoices.service.InvoiceService;
 import com.marcobehler.myfancypdfinvoices.dto.InvoiceDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 // tag::restControllerAnnotation[]
@@ -34,14 +34,10 @@ public class MyFancyPdfInvoicesController {
     }
     // end::invoiceMethod[]
 
-    // tag::postAnnotation[]
+    // tag::createInvoiceMethod[]
     @PostMapping("/invoices")
-    // end::postAnnotation[]
-    // tag::postMethodSignature[]
-    public Invoice createInvoice(@RequestBody @Valid InvoiceDto invoiceDto) {
-    // end::postMethodSignature[]
-    // tag::postDelegate[]
-        return invoiceService.create(invoiceDto.getUserId(), invoiceDto.getAmount());
-    // end::postDelegate[]
+    public Invoice createInvoice(@RequestParam("user_id") @NotBlank String userId, @RequestParam @Min(10) @Max(50) Integer amount) {
+        return invoiceService.create(userId, amount);
     }
+    // end::createInvoiceMethod[]
 }
