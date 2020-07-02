575ae7821a2f96a2127e87eef1d989b0d9cfe599
Marco Behler <marco@marcobehler.com>
2020-07-02 16:53:04 +0100

    Spring Web MVC HTML

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java
@@ -1,9 +1,12 @@
 package com.marcobehler.myfancypdfinvoices.web;
 
 import com.marcobehler.myfancypdfinvoices.web.forms.LoginForm;
+import org.apache.juli.logging.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
@@ -30,5 +33,19 @@ public class WebsiteController {
         model.addAttribute("loginForm", new LoginForm());
         // end::loginModelAttributes[]
         return "login.html";
+    }
+
+    // tag::loginPostMethodDescription[]
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginForm loginForm){
+        // end::loginPostMethodDescription[]
+        // tag::loginPostModelAttributes[]
+        if (loginForm.getUsername().equals(loginForm.getPassword())) {
+            return "redirect:/";
+        }
+        // end::loginPostModelAttributes[]
+        // tag::loginPostReturn[]
+        return "redirect:login.html?error";
+        // tag::loginPostReturn[]
     }
 }
