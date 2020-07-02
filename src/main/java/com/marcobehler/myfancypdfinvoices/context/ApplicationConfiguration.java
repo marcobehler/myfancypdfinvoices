1913f8efd6365e1f4fdf339b6d1b8a569db47ba0
Marco Behler <marco@marcobehler.com>
2020-07-02 10:34:31 +0100

    Spring Web MVC HTML

--- src/main/java/com/marcobehler/myfancypdfinvoices/context/ApplicationConfiguration.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/context/ApplicationConfiguration.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
@@ -31,4 +34,35 @@ public class ApplicationConfiguration {
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
     }
+
+    // tag::viewResolver[]
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+
+        viewResolver.setOrder(1); // optional
+        viewResolver.setViewNames(new String[] {"*.html", "*.xhtml"}); // optional
+        return viewResolver;
+    }
+    // end::viewResolver[]
+
+    // tag::templateEngine[]
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+    // end::templateEngine[]
+
+    // tag::templateResolver[]
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
+    // end::templateResolver[]
 }
