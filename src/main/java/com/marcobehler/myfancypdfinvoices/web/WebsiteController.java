b0d89915ebe979f05e58d9383088e192cdb8f313
Marco Behler <marco@marcobehler.com>
2020-07-02 12:29:58 +0100

    Spring Web MVC HTML

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java
@@ -1,17 +1,23 @@
 package com.marcobehler.myfancypdfinvoices.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 @Controller
 public class WebsiteController {
 
     // tag::homePageMethodDescription[]
     @GetMapping("/")
-    public String homepage() {
+    public String homepage(Model model, @RequestParam(required = false) String username) {
         // end::homePageMethodDescription[]
-        // tag::templateReturn[]
+        // tag::modelAttributes[]
+        model.addAttribute("username", username == null ? username : "stranger");
+        model.addAttribute("currentDate", new Date());
+        // end::modelAttributes[]
         return "index.html";
-        // end::templateReturn[]
     }
 }
