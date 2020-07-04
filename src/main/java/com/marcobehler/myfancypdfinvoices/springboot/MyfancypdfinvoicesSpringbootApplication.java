a0240c226827661a576bc210f91075283052d823
Marco Behler <marco@marcobehler.com>
2020-07-04 15:37:54 +0100

    spring boot project

---
+++ src/main/java/com/marcobehler/myfancypdfinvoices/springboot/MyfancypdfinvoicesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.marcobehler.myfancypdfinvoices.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfancypdfinvoicesSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfancypdfinvoicesSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcobehler/myfancypdfinvoices/springboot/MyfancypdfinvoicesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcobehler.myfancypdfinvoices.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfancypdfinvoicesSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
