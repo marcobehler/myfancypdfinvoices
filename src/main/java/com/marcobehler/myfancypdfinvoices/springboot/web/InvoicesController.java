a9cbe6dbcff3e7d0fefc07f3b078c3a17c776d72
Marco Behler <marco@marcobehler.com>
2020-07-04 16:09:20 +0100

    `Spring Boot`

--- src/main/java/com/marcobehler/myfancypdfinvoices/springboot/web/InvoicesController.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/springboot/web/InvoicesController.java
@@ -2,22 +2,19 @@
 package com.marcobehler.myfancypdfinvoices.springboot.web;
 
 
+import com.marcobehler.myfancypdfinvoices.springboot.dto.InvoiceDto;
 import com.marcobehler.myfancypdfinvoices.springboot.model.Invoice;
 import com.marcobehler.myfancypdfinvoices.springboot.service.InvoiceService;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
+import javax.validation.Valid;
 import java.util.List;
 
 // tag::restControllerAnnotation[]
 @RestController
-@Validated
 public class InvoicesController {
 // end::restControllerAnnotation[]
 // end::beginAnnotation[]
@@ -42,8 +39,8 @@ public class InvoicesController {
 
     // tag::createInvoiceMethod[]
     @PostMapping("/invoices")
-    public Invoice createInvoice(@RequestParam("user_id") @NotBlank String userId, @RequestParam @Min(10) @Max(50) Integer amount) {
-        return invoiceService.create(userId, amount);
+    public Invoice createInvoice(@Valid @RequestBody InvoiceDto invoiceDto) {
+        return invoiceService.create(invoiceDto.getUserId(), invoiceDto.getAmount());
     }
     // end::createInvoiceMethod[]
 }
