6c0c2c912ff0edcc1fe6e205fc3919d274099b00
Marco Behler <marco@marcobehler.com>
2020-07-04 10:16:52 +0100

    Spring Databases

--- src/main/java/com/marcobehler/myfancypdfinvoices/context/ApplicationConfiguration.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/context/ApplicationConfiguration.java
@@ -2,6 +2,7 @@ package com.marcobehler.myfancypdfinvoices.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.marcobehler.myfancypdfinvoices.ApplicationLauncher;
+import org.h2.jdbcx.JdbcDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
@@ -29,6 +32,24 @@ public class ApplicationConfiguration {
     }
     // end::methodValidationPostProcessor[]
 
+    // tag::dataSource[]
+    @Bean
+    public DataSource dataSource() {
+        // tag::newDataSourceLine[]
+        JdbcDataSource ds = new JdbcDataSource();
+        // end::newDataSourceLine[]
+        // tag::dataSourceUrlLine[]
+        ds.setURL("jdbc:h2:~/myfirstspringdatabase");
+        // end::dataSourceUrlLine[]
+        // tag::dataSourceUserLine[]
+        ds.setUser("sa");
+        // end::dataSourceUserLine[]
+        // tag::dataSourcePasswordLine[]
+        ds.setPassword("sa");
+        // end::dataSourcePasswordLine[]
+        return ds;
+    }
+    // end::dataSource[]
 
     @Bean
     public ObjectMapper objectMapper() {
