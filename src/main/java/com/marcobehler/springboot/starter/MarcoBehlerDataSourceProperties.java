1936ad3a70b070b0e53c26f8408c1d5b2c598a7c
Marco Behler <marco@marcobehler.com>
2020-07-06 14:07:49 +0100

    AutoConfigurations

---
+++ src/main/java/com/marcobehler/springboot/starter/MarcoBehlerDataSourceProperties.java
@@ -0,0 +1,53 @@
+package com.marcobehler.springboot.starter;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "marcobehler.datasource")
+public class MarcoBehlerDataSourceProperties {
+
+    private String username;
+    private String password;
+    private String type;
+    private String url;
+    private String driverClassName;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+}
