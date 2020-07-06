d38ea9da8eb0c25048bdbcc9793a826169727ea0
Marco Behler <marco@marcobehler.com>
2020-07-06 15:25:20 +0100

    AutoConfigurations

--- src/main/java/com/marcobehler/myfancypdfinvoices/springboot/MyfancypdfinvoicesSpringbootApplication.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/springboot/MyfancypdfinvoicesSpringbootApplication.java
@@ -1,10 +1,22 @@
 package com.marcobehler.myfancypdfinvoices.springboot;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class MyfancypdfinvoicesSpringbootApplication {
+
+	@Bean
+	public InitializingBean runner(DataSource dataSource) {
+		return () -> {
+			System.out.println("This is the datasource this Spring Boot project is using: " + dataSource);
+		};
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyfancypdfinvoicesSpringbootApplication.class, args);
