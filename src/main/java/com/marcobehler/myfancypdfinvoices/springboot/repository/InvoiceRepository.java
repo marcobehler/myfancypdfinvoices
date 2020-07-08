580606e8141496012db435b3155531273692b66d
Marco Behler <marco@marcobehler.com>
2020-07-08 10:02:15 +0100

    Spring Data JDBC

--- src/main/java/com/marcobehler/myfancypdfinvoices/springboot/repository/InvoiceRepository.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/springboot/repository/InvoiceRepository.java
@@ -1,7 +1,16 @@
 package com.marcobehler.myfancypdfinvoices.springboot.repository;
 
 import com.marcobehler.myfancypdfinvoices.springboot.model.Invoice;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface InvoiceRepository extends CrudRepository<Invoice, String> {
+
+    // tag::queryAnnotation[]
+    @Query("SELECT id, pdf_url, user_id, amount FROM invoices where user_id = :userId")
+    // end::queryAnnotation[]
+    // tag::method[]
+    Iterable<Invoice> findByUserId(@Param("userId") String userId);
+    // end::method[]
 }
