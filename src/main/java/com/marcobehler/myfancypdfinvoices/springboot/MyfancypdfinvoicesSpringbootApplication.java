f17c79ce96fa3f34794c2ba08c4e52bd8e1ea859
Marco Behler <marco@marcobehler.com>
2021-10-10 15:55:39 +0200

    Spring Boot upgrade

--- src/main/java/com/marcobehler/myfancypdfinvoices/springboot/MyfancypdfinvoicesSpringbootApplication.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/springboot/MyfancypdfinvoicesSpringbootApplication.java
@@ -1,11 +1,7 @@
 package com.marcobehler.myfancypdfinvoices.springboot;
 
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class MyfancypdfinvoicesSpringbootApplication {
