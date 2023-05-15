739430b7b6b0c272b89a4ee7094c99cf449182a1
Marco Behler <marco@marcobehler.com>
2023-05-15 19:26:49 +0200

    Updated to Spring 3

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/GlobalExceptionHandler.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.validation.ConstraintViolationException;
+import jakarta.validation.ConstraintViolationException;
 
 
 // tag::adviceAnnotation[]
