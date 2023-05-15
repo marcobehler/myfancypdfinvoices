35538bc6e61275598b3edef98277afd21251f754
Marco Behler <marco@marcobehler.com>
2023-05-15 19:18:49 +0200

    Updated to Spring 3

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFancyPdfInvoicesServlet.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFancyPdfInvoicesServlet.java
@@ -7,10 +7,10 @@ import com.marcobehler.myfancypdfinvoices.services.InvoiceService;
 import com.marcobehler.myfancypdfinvoices.services.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
