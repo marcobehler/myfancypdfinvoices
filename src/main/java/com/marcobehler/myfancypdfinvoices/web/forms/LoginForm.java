67f5f2e9eef74c549ec503dac7ad89a5de935d45
Marco Behler <marco@marcobehler.com>
2023-05-15 19:35:36 +0200

    Updated to Spring 3

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/forms/LoginForm.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/forms/LoginForm.java
@@ -1,7 +1,7 @@
 package com.marcobehler.myfancypdfinvoices.web.forms;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class LoginForm {
 
