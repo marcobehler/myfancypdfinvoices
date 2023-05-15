02e2658cbecdb3fd7b57c08e8957008e1e4a01e4
Marco Behler <marco@marcobehler.com>
2023-05-15 19:10:10 +0200

    Updated to Spring 3

--- src/main/java/com/marcobehler/MyFirstServlet.java
+++ src/main/java/com/marcobehler/MyFirstServlet.java
@@ -1,8 +1,8 @@
 package com.marcobehler;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class MyFirstServlet extends HttpServlet {
