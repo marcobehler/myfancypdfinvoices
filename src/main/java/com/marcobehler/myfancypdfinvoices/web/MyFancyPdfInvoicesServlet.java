a6e197f6047fc7b31d129f4f4ff37e907320ec90
Marco Behler <marco@marcobehler.com>
2023-05-20 21:00:15 +0200

    created 1-03-03

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFancyPdfInvoicesServlet.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFancyPdfInvoicesServlet.java
@@ -3,9 +3,9 @@ package com.marcobehler.myfancypdfinvoices.web;
 import com.marcobehler.myfancypdfinvoices.context.Application;
 import com.marcobehler.myfancypdfinvoices.model.Invoice;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
