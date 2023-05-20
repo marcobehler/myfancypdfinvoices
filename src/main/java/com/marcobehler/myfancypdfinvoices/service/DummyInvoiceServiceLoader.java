9c6f48069680de3e1ff9b94b98153c06f122971b
Marco Behler <marco@marcobehler.com>
2023-05-20 21:27:33 +0200

    created 2-01-05

--- src/main/java/com/marcobehler/myfancypdfinvoices/service/DummyInvoiceServiceLoader.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/service/DummyInvoiceServiceLoader.java
@@ -5,20 +5,29 @@ import org.springframework.stereotype.Service;
 
 import jakarta.annotation.PostConstruct;
 
+// tag::serviceAnnotation[]
 @Service
+// end::serviceAnnotation[]
+// tag::profileAnnotation[]
 @Profile("dev")
+// end::profileAnnotation[]
 public class DummyInvoiceServiceLoader {
 
+
+    // tag::constructorDi[]
     private final InvoiceService invoiceService;
 
     public DummyInvoiceServiceLoader(InvoiceService invoiceService) {
         this.invoiceService = invoiceService;
     }
+    // end::constructorDi[]
 
+    // tag::setup[]
     @PostConstruct
     public void setup() {
         System.out.println("Creating dev invoices...");
         invoiceService.create("someUserId", 50);
         invoiceService.create("someOtherUserId", 100);
     }
+    // end::setup[]
 }
