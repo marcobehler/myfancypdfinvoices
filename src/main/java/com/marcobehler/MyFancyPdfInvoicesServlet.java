1a3a2a0803422800ea3225334c77f77311a5090c
Marco Behler <marco@marcobehler.com>
2023-05-20 20:56:11 +0200

    created 1-03-2

--- src/main/java/com/marcobehler/MyFancyPdfInvoicesServlet.java
+++ src/main/java/com/marcobehler/MyFancyPdfInvoicesServlet.java
@@ -2,9 +2,9 @@ package com.marcobehler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
