05e00b26ae269813c1fdd1e8c7dc60f2ae68ac2e
Marco Behler <marco@marcobehler.com>
2023-05-22 18:47:54 +0200

    #5-01-03

--- src/main/java/com/marcobehler/myfancypdfinvoices/context/ApplicationConfiguration.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/context/ApplicationConfiguration.java
@@ -13,9 +13,9 @@ import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
 
