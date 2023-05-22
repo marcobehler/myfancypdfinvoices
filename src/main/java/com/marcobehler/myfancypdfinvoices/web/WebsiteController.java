d865d824a9289d3f8ae95ae556ed4d258ab36d6a
Your Name <you@example.com>
2023-05-22 15:31:20 +0200

    #4-01-06

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java
@@ -1,7 +1,6 @@
 package com.marcobehler.myfancypdfinvoices.web;
 
 import com.marcobehler.myfancypdfinvoices.web.forms.LoginForm;
-import org.apache.juli.logging.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,7 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.validation.Valid;
-import java.util.Date;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class WebsiteController {
@@ -22,7 +22,7 @@ public class WebsiteController {
         // end::homePageMethodDescription[]
         // tag::modelAttributes[]
         model.addAttribute("username", username);
-        model.addAttribute("currentDate", new Date());
+        model.addAttribute("currentDate", LocalDateTime.now());
         // end::modelAttributes[]
         return "index.html";
     }
