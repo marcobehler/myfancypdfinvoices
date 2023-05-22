11f31d22878fe888d2bcb58d5a63ff31bee83da5
Marco Behler <marco@marcobehler.com>
2023-05-22 14:16:21 +0200

    created 3-01-03

--- src/main/java/com/marcobehler/myfancypdfinvoices/ApplicationLauncher.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/ApplicationLauncher.java
@@ -9,7 +9,7 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletContext;
+import jakarta.servlet.ServletContext;
 
 public class ApplicationLauncher {
 
