5d1e9cdfcb8bbbd8259f194baf88a1d549567d7b
Marco Behler <marco@marcobehler.com>
2023-05-22 14:42:07 +0200

    created 4 01 03

--- src/main/java/com/marcobehler/myfancypdfinvoices/service/InvoiceService.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/service/InvoiceService.java
@@ -6,8 +6,8 @@ import com.marcobehler.myfancypdfinvoices.model.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
