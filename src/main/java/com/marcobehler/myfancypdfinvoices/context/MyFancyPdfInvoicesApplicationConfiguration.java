a061ee28248854f279193028468b152e798ff46c
marco.behler <marco@marcobehler.com>
2026-06-11 21:07:27 +0200

    csp-2026-ch04-020-spring-mvc-writing: checkpoint from #03-01-spring-mvc-writing-88d533
    Course checkpoint migration for csp-2026.
    
    Old ref: #03-01-spring-mvc-writing-88d533
    First use: 04-spring-webmvc-rest-services.adoc:53
    Included paths:
    - src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFirstSpringController.java

--- src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -6,26 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
+// tag::propertySourceAnnotation[]
 @PropertySource("classpath:/application.properties")
 @PropertySource(value = "classpath:/application-${spring.profiles.active}.properties"
                     , ignoreResourceNotFound = true)
-// tag::enableWebMVCAnnotation[]
-@EnableWebMvc
-public class ApplicationConfiguration {
-// end::enableWebMVCAnnotation[]
-
-    // tag::methodValidationPostProcessor[]
-    @Bean // <1>
-    public MethodValidationPostProcessor methodValidationPostProcessor() {
-        return new MethodValidationPostProcessor();
-    }
-    // end::methodValidationPostProcessor[]
-
+public class MyFancyPdfInvoicesApplicationConfiguration {
+// end::propertySourceAnnotation[]
 
     @Bean
     public ObjectMapper objectMapper() {
