442b5bf5563eaf21e9df0180457d8404f3da4385
marco.behler <marco@marcobehler.com>
2026-06-11 21:07:27 +0200

    csp-2026-ch04-040-handling: checkpoint from #3-01-01-handling-5d627f
    Course checkpoint migration for csp-2026.
    
    Old ref: #3-01-01-handling-5d627f
    First use: 04-spring-webmvc-rest-services.adoc:183
    Included paths:
    - src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFancyPdfInvoicesController.java

---
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFancyPdfInvoicesController.java
@@ -0,0 +1,33 @@
+package com.marcobehler.myfancypdfinvoices.web;
+
+import com.marcobehler.myfancypdfinvoices.model.Invoice;
+import com.marcobehler.myfancypdfinvoices.service.InvoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+// tag::restControllerAnnotation[]
+@RestController
+public class MyFancyPdfInvoicesController {
+// end::restControllerAnnotation[]
+
+    // tag::invoiceServiceInjection[]
+    private final InvoiceService invoiceService;
+
+    public MyFancyPdfInvoicesController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+    // end::invoiceServiceInjection[]
+
+    // tag::getMethod[]
+    @GetMapping("/invoices")
+    // @RequestMapping(value = "/invoices", method = RequestMethod.GET)
+    public List<Invoice> invoices() {
+        return invoiceService.findAll();
+    }
+    // end::getMethod[]
+}
