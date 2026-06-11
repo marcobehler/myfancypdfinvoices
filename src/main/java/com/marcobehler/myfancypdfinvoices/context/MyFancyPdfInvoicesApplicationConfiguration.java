925a363f0e27e8f14bee6581f9315e2111bc1a89
marco.behler <marco@marcobehler.com>
2026-06-11 15:54:07 +0200

    csp-2026-ch03-080-prototypes: checkpoint from #2-01-02-prototypes-9bf92d
    Course checkpoint migration for csp-2026.
    
    Old ref: #2-01-02-prototypes-9bf92d
    First use: 03-spring-ioc-basics.adoc:218
    Included paths:
    - src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
    - src/main/java/com/marcobehler/myfancypdfinvoices/service/InvoiceService.java
    - src/main/java/com/marcobehler/myfancypdfinvoices/web/MyFancyPdfInvoicesServlet.java

--- src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -3,14 +3,17 @@ package com.marcobehler.myfancypdfinvoices.context;
 import tools.jackson.databind.ObjectMapper;
 import com.marcobehler.myfancypdfinvoices.service.InvoiceService;
 import com.marcobehler.myfancypdfinvoices.service.UserService;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class MyFancyPdfInvoicesApplicationConfiguration {
 
     // tag::userServiceMethod[]
     @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public UserService userService() {
         return new UserService();
     }
