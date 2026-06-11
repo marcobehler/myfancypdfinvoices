4399a639ae14c9374f29a14340fd2521b62b2957
marco.behler <marco@marcobehler.com>
2026-06-11 21:07:27 +0200

    csp-2026-ch04-170-step-3-01-07: checkpoint from #3-01-07
    Course checkpoint migration for csp-2026.
    
    Old ref: #3-01-07
    First use: 04-spring-webmvc-rest-services.adoc:918
    Included paths:
    - src/main/java/com/marcobehler/myfancypdfinvoices/web/GlobalControllerExceptionHandler.java

---
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/GlobalControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.marcobehler.myfancypdfinvoices.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.validation.ConstraintViolationException;
+
+
+@RestControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handlemethodArgumentNotValid(MethodArgumentNotValidException exception) { // <4>
+        // TODO you can choose to return your custom object here, which will then get transformed to json/xml etc.
+        return "Sorry, that was not quite right: " + exception.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public String handleConstraintViolation(ConstraintViolationException exception) { // <4>
+        // TODO you can choose to return your custom object here, which will then get transformed to json/xml etc.
+        return "Sorry, that was not quite right: " + exception.getMessage();
+    }
+}
