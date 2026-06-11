9d6c506984d4aebb30cd9ee6c7455496fbe3eb1d
marco.behler <marco@marcobehler.com>
2026-06-11 21:07:27 +0200

    csp-2026-ch04-070-adding: checkpoint from #3-01-01-adding-2730c4
    Course checkpoint migration for csp-2026.
    
    Old ref: #3-01-01-adding-2730c4
    First use: 04-spring-webmvc-rest-services.adoc:295
    Included paths:
    - src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java

--- src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -6,15 +6,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
-// tag::propertySourceAnnotation[]
 @PropertySource("classpath:/application.properties")
 @PropertySource(value = "classpath:/application-${spring.profiles.active}.properties"
                     , ignoreResourceNotFound = true)
+// tag::enableWebMVCAnnotation[]
+@EnableWebMvc
 public class MyFancyPdfInvoicesApplicationConfiguration {
-// end::propertySourceAnnotation[]
+// end::enableWebMVCAnnotation[]
 
     @Bean
     public ObjectMapper objectMapper() {
