e5f5914927e871ab37893028969a793073063eec
marco.behler <marco@marcobehler.com>
2026-06-11 21:07:28 +0200

    csp-2026-ch05-110-checkpoint: checkpoint from #4-01-05-checkpoint-982a29
    Course checkpoint migration for csp-2026.
    
    Old ref: #4-01-05-checkpoint-982a29
    First use: 05-spring-webmvc-html-pages.adoc:453
    Included paths:
    - src/main/resources/templates/login.html

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java
@@ -1,6 +1,7 @@
 package com.marcobehler.myfancypdfinvoices.web;
 
 import com.marcobehler.myfancypdfinvoices.web.forms.LoginForm;
+import org.apache.juli.logging.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @Controller
 public class WebsiteController {
@@ -19,7 +20,7 @@ public class WebsiteController {
         // end::homePageMethodDescription[]
         // tag::modelAttributes[]
         model.addAttribute("username", username);
-        model.addAttribute("currentDate", LocalDateTime.now());
+        model.addAttribute("currentDate", new Date());
         // end::modelAttributes[]
         return "index.html";
     }
