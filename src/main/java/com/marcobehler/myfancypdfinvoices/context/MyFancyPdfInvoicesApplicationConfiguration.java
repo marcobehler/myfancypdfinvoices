c74e5f27b580578b43043628152501a651c33de9
marco.behler <marco@marcobehler.com>
2026-06-11 21:07:27 +0200

    csp-2026-ch03-120-componentscan: checkpoint from #2-01-02-componentscan-bef1e0
    Course checkpoint migration for csp-2026.
    
    Old ref: #2-01-02-componentscan-bef1e0
    First use: 03-spring-ioc-basics.adoc:429
    Included paths:
    - src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java

--- src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 // tag::componentScanAnnotation[]
-@ComponentScan
+@ComponentScan(basePackageClasses = ApplicationLauncher.class)
 public class MyFancyPdfInvoicesApplicationConfiguration {
 // end::componentScanAnnotation[]
 
