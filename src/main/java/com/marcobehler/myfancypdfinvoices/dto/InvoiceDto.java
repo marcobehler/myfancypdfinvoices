859e230fc165a913becbfaaf271159f4fc5b6399
marco.behler <marco@marcobehler.com>
2026-06-11 21:07:27 +0200

    csp-2026-ch04-110-step-3-01-02: checkpoint from #3-01-02
    Course checkpoint migration for csp-2026.
    
    Old ref: #3-01-02
    First use: 04-spring-webmvc-rest-services.adoc:700
    Included paths:
    - src/main/java/com/marcobehler/myfancypdfinvoices/dto/InvoiceDto.java

--- src/main/java/com/marcobehler/myfancypdfinvoices/dto/InvoiceDto.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/dto/InvoiceDto.java
@@ -2,11 +2,18 @@ package com.marcobehler.myfancypdfinvoices.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public class InvoiceDto {
 
     @JsonProperty("user_id")
+    @NotBlank
     private String userId;
 
+    @Min(10)
+    @Max(50)
     private Integer amount;
 
     public String getUserId() {
