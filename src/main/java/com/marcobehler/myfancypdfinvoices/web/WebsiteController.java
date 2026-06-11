9f78f12c75b5f921afdf36e3caef56aa4dd6edfa
marco.behler <marco@marcobehler.com>
2026-06-11 21:07:27 +0200

    csp-2026-ch05-090-step-4-01-04: checkpoint from #4-01-04
    Course checkpoint migration for csp-2026.
    
    Old ref: #4-01-04
    First use: 05-spring-webmvc-html-pages.adoc:385
    Included paths:
    - src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java
@@ -1,11 +1,12 @@
 package com.marcobehler.myfancypdfinvoices.web;
 
+import com.marcobehler.myfancypdfinvoices.web.forms.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Controller
 public class WebsiteController {
@@ -16,8 +17,18 @@ public class WebsiteController {
         // end::homePageMethodDescription[]
         // tag::modelAttributes[]
         model.addAttribute("username", username);
-        model.addAttribute("currentDate", new Date());
+        model.addAttribute("currentDate", LocalDateTime.now());
         // end::modelAttributes[]
         return "index.html";
+    }
+
+    // tag::loginMethodDescription[]
+    @GetMapping("/login")
+    public String login(Model model){
+        // end::loginMethodDescription[]
+        // tag::loginModelAttributes[]
+        model.addAttribute("loginForm", new LoginForm());
+        // end::loginModelAttributes[]
+        return "login.html";
     }
 }
