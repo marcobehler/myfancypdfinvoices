d66be9b33ccbefdcefb9fec61740001560641ca7
marco.behler <marco@marcobehler.com>
2026-06-11 21:07:28 +0200

    csp-2026-ch06-050-creating: checkpoint from #5-01-00-creating-9012d8
    Course checkpoint migration for csp-2026.
    
    Old ref: #5-01-00-creating-9012d8
    First use: 06-spring-database-access.adoc:166
    Included paths:
    - src/main/java/com/marcobehler/myfancypdfinvoices/context/ApplicationConfiguration.java

--- src/main/java/com/marcobehler/myfancypdfinvoices/service/InvoiceService.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/service/InvoiceService.java
@@ -2,35 +2,29 @@ package com.marcobehler.myfancypdfinvoices.service;
 
 
 import com.marcobehler.myfancypdfinvoices.model.Invoice;
+import com.marcobehler.myfancypdfinvoices.model.User;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
 import java.util.List;
-import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class InvoiceService {
 
-    private final JdbcTemplate jdbcTemplate;
+    private List<Invoice> invoices = new CopyOnWriteArrayList<>();
 
     private final UserService userService;
-
+    // tag::cdnUrlConstructor[]
     private final String cdnUrl;
 
-    // tag::jdbcTemplateConstructor[]
-    public InvoiceService(UserService userService, JdbcTemplate jdbcTemplate, @Value("${cdn.url}") String cdnUrl) {
+    public InvoiceService(UserService userService, @Value("${cdn.url}") String cdnUrl) {
         this.userService = userService;
         this.cdnUrl = cdnUrl;
-        this.jdbcTemplate = jdbcTemplate;
     }
-    // end::jdbcTemplateConstructor[]
+    // end::cdnUrlConstructor[]
 
     // tag::postConstruct[]
     @PostConstruct
@@ -48,54 +42,21 @@ public class InvoiceService {
     }
     // end::preDestroy[]
 
-    // tag::findAllMethod[]
     public List<Invoice> findAll() {
-        return jdbcTemplate.query("select id, user_id, pdf_url, amount from invoices", (resultSet, rowNum) -> {
-            Invoice invoice = new Invoice();
-            invoice.setId(resultSet.getObject("id").toString());
-            invoice.setPdfUrl(resultSet.getString("pdf_url"));
-            invoice.setUserId(resultSet.getString("user_id"));
-            invoice.setAmount(resultSet.getInt("amount"));
-            return invoice;
-        });
+        return invoices;
     }
-    // end::findAllMethod[]
 
-    // tag::createMethod[]
     public Invoice create(String userId, Integer amount) {
-        // tag::createStaticPdfUrl[]
-        String generatedPdfUrl = cdnUrl + "/images/default/sample.pdf";
-        // end::createStaticPdfUrl[]
+        User user = userService.findById(userId);
+        if (user == null) {
+            throw new IllegalStateException();
+        }
 
-        // tag::createKeyholder[]
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        // end::createKeyholder[]
-
-        // tag::jdbcTemplateUpdate[]
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection
-                    .prepareStatement("insert into invoices (user_id, pdf_url, amount) values (?, ?, ?)",
-                            Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, userId);  // <3>
-            ps.setString(2, generatedPdfUrl);
-            ps.setInt(3, amount);
-            return ps;
-        }, keyHolder);
-        // end::jdbcTemplateUpdate[]
-
-        // tag::uuid[]
-        String uuid = !keyHolder.getKeys().isEmpty() ? ((KeyHolder) keyHolder.getKeys().values().iterator().next()).toString()
-                : null;
-        // end::uuid[]
-
-        // tag::invoicePojo[]
-        Invoice invoice = new Invoice();
-        invoice.setId(uuid);
-        invoice.setPdfUrl(generatedPdfUrl);
-        invoice.setAmount(amount);
-        invoice.setUserId(userId);
+        // tag::cdnUrlUsed[]
+        // TODO real pdf creation and storing it on network server
+        Invoice invoice = new Invoice(userId, amount, cdnUrl + "/images/default/sample.pdf");
+        invoices.add(invoice);
         return invoice;
-        // end::invoicePojo[]
+        // end::cdnUrlUsed[]
     }
-    // end::createMethod[]
 }
