e9d631f808888789504c781f202965c7f4a4442e
marco.behler <marco@marcobehler.com>
2026-06-11 21:07:27 +0200

    csp-2026-ch05-030-spring-mvc-html: checkpoint from #04-01-spring-mvc-html
    Course checkpoint migration for csp-2026.
    
    Old ref: #04-01-spring-mvc-html
    First use: 05-spring-webmvc-html-pages.adoc:112
    Included paths:
    - pom.xml

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java
@@ -1,17 +1,59 @@
 package com.marcobehler.myfancypdfinvoices.web;
 
+import com.marcobehler.myfancypdfinvoices.web.forms.LoginForm;
+import org.apache.juli.logging.Log;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
+import java.util.Date;
 
 @Controller
 public class WebsiteController {
 
     // tag::homePageMethodDescription[]
     @GetMapping("/")
-    public String homepage() {
+    public String homepage(Model model, @RequestParam(required = false, defaultValue = "stranger") String username) {
         // end::homePageMethodDescription[]
-        // tag::templateReturn[]
+        // tag::modelAttributes[]
+        model.addAttribute("username", username);
+        model.addAttribute("currentDate", new Date());
+        // end::modelAttributes[]
         return "index.html";
-        // end::templateReturn[]
+    }
+
+    // tag::loginMethodDescription[]
+    @GetMapping("/login")
+    public String login(Model model){
+        // end::loginMethodDescription[]
+        // tag::loginModelAttributes[]
+        model.addAttribute("loginForm", new LoginForm());
+        // end::loginModelAttributes[]
+        return "login.html";
+    }
+
+    // tag::loginPostMethodDescription[]
+    @PostMapping("/login")
+    public String login(@ModelAttribute @Valid LoginForm loginForm,BindingResult bindingResult,  Model model ){
+    // end::loginPostMethodDescription[]
+    // tag::bindingResult[]
+        if (bindingResult.hasErrors()) {
+            return "login.html";
+        }
+    // end::bindingResult[]
+        // tag::loginPostModelAttributes[]
+        if (loginForm.getUsername().equals(loginForm.getPassword())) {
+            return "redirect:/";
+        }
+        // end::loginPostModelAttributes[]
+        // tag::loginPostReturn[]
+        model.addAttribute("invalidCredentials", "true");
+        return "login.html";
+        // tag::loginPostReturn[]
     }
 }
