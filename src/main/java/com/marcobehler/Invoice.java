c77b56196ad795168daa2f617ab8d610b1c29425
marco.behler <marco@marcobehler.com>
2026-06-11 15:54:07 +0200

    csp-2026-ch02-050-creating: checkpoint from #1-03-01-creating-6a30d1
    Course checkpoint migration for csp-2026.
    
    Old ref: #1-03-01-creating-6a30d1
    First use: 02-java-webapps-without-spring.adoc:619
    Included paths:
    - src/main/java/com/marcobehler/Invoice.java
    - src/main/java/com/marcobehler/InvoiceService.java

---
+++ src/main/java/com/marcobehler/Invoice.java
@@ -0,0 +1,52 @@
+package com.marcobehler;
+
+import java.util.UUID;
+
+public class Invoice {
+
+    private String id, userId, pdfUrl;
+
+    private Integer amount;
+
+    public Invoice() {
+    }
+
+    public Invoice(String userId, Integer amount, String pdfUrl) {
+        this.id = UUID.randomUUID().toString();
+        this.userId = userId;
+        this.pdfUrl = pdfUrl;
+        this.amount = amount;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPdfUrl() {
+        return pdfUrl;
+    }
+
+    public void setPdfUrl(String pdfUrl) {
+        this.pdfUrl = pdfUrl;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
