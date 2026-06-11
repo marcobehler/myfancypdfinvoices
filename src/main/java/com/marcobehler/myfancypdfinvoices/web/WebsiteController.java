6372bc8dccc7608632bf7c630ec06eccd8323a74
marco.behler <marco@marcobehler.com>
2026-06-11 21:07:27 +0200

    csp-2026-ch05-060-step-4-01-03: checkpoint from #4-01-03
    Course checkpoint migration for csp-2026.
    
    Old ref: #4-01-03
    First use: 05-spring-webmvc-html-pages.adoc:229
    Included paths:
    - src/main/resources/templates/index.html
    - src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/WebsiteController.java
@@ -1,17 +1,23 @@
 package com.marcobehler.myfancypdfinvoices.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class WebsiteController {
 
     // tag::homePageMethodDescription[]
     @GetMapping("/")
-    public String homepage() {
+    public String homepage(Model model, @RequestParam(required = false, defaultValue = "stranger") String username) {
         // end::homePageMethodDescription[]
-        // tag::templateReturn[]
+        // tag::modelAttributes[]
+        model.addAttribute("username", username);
+        model.addAttribute("currentDate", LocalDateTime.now());
+        // end::modelAttributes[]
         return "index.html";
-        // end::templateReturn[]
     }
 }
