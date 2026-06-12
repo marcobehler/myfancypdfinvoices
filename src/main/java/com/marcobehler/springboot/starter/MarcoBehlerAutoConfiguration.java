292f5857d891fa080749629423de29a44bf4c83f
marco.behler <marco@marcobehler.com>
2026-06-12 13:23:02 +0200

    csp-2026-ch08-010-autoconfigurations-boot-4: checkpoint from #07-01-autoconfigurations-boot-4
    Course checkpoint migration for csp-2026.
    
    Old ref: #07-01-autoconfigurations-boot-4
    First use: 08-spring-boot-internals.adoc:47
    Included paths:
    - pom.xml
    - src/main/java/com/marcobehler/springboot/starter/MarcoBehlerAutoConfiguration.java
    - src/main/resources/META-INF/spring/org.springframework.boot.autoconfigure.AutoConfiguration.imports

---
+++ src/main/java/com/marcobehler/springboot/starter/MarcoBehlerAutoConfiguration.java
@@ -0,0 +1,36 @@
+package com.marcobehler.springboot.starter;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.vibur.dbcp.ViburDBCPDataSource;
+import org.vibur.dbcp.ViburDataSource;
+
+import javax.sql.DataSource;
+
+// tag::springBootAnnotations[]
+@AutoConfiguration(before = DataSourceAutoConfiguration.class)  // <1>
+@ConditionalOnClass(ViburDataSource.class)
+@ConditionalOnMissingBean(DataSource.class)
+// end::springBootAnnotations[]
+// tag::configurationProperties[]
+@EnableConfigurationProperties(MarcoBehlerDataSourceProperties.class)
+public class MarcoBehlerAutoConfiguration {
+// end::configurationProperties[]
+
+    // tag::dataSource[]
+    @Bean
+    public ViburDBCPDataSource dataSource(MarcoBehlerDataSourceProperties properties) {
+        ViburDBCPDataSource ds = new ViburDBCPDataSource();
+        ds.setJdbcUrl(properties.getUrl());
+        ds.setUsername(properties.getUsername());
+        ds.setPassword(properties.getPassword());
+        ds.setDriverClassName(properties.getDriverClassName());
+        ds.start();
+        return ds;
+    }
+    // end::dataSource[]
+}
---
+++ src/main/java/com/marcobehler/springboot/starter/MarcoBehlerDataSourceProperties.java
@@ -0,0 +1,48 @@
+package com.marcobehler.springboot.starter;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+// tag::autoConfigurationHeader[]
+@ConfigurationProperties(prefix = "marcobehler.datasource")
+public class MarcoBehlerDataSourceProperties {
+// end::autoConfigurationHeader[]
+
+    // tag::autoConfigurationFields[]
+    private String username;
+    private String password;
+    private String url;
+    private String driverClassName;
+    // end::autoConfigurationFields[]
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+}
